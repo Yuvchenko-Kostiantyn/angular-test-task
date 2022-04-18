8b8f16609ed70dce571f8cac10cf9d672a9f28a8
Kostiantyn Yuvchenko <kostiantyn.yuvchenko@starz.com>
2022-04-18 16:58:27 +0300

    feat(gallery): add infinite scroll to photos page and cover it with tests as best I could

--- src/app/gallery/components/photos/photos.component.spec.ts
+++ src/app/gallery/components/photos/photos.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PhotosComponent } from './photos.component';
+import { MatCardModule } from "@angular/material/card";
+import { GalleryService } from "../../services/gallery.service";
+import { cold } from "jasmine-marbles";
+
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
 
 describe('PhotosComponent', () => {
   let component: PhotosComponent;
   let fixture: ComponentFixture<PhotosComponent>;
+  let mockGalleryService: SpyObj<GalleryService>;
+  let observerSpy: SpyObj<IntersectionObserver>;
 
   beforeEach(async () => {
+    spyOn(Math, 'random').and.returnValue(200);
+    observerSpy = createSpyObj(IntersectionObserver, ['observe', 'disconnect']);
+    mockGalleryService = createSpyObj('GalleryService', [ 'getPage', 'addFavorite' ]);
+    mockGalleryService.getPage.and.returnValue(cold('a', { a: [1, 2]}));
+
     await TestBed.configureTestingModule({
-      declarations: [ PhotosComponent ]
+      declarations: [ PhotosComponent ],
+      imports: [ MatCardModule ],
+      providers: [{ provide: GalleryService, useValue: mockGalleryService }]
     })
     .compileComponents();
   });
@@ -21,5 +36,24 @@ describe('PhotosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add image to favorites', () => {
+    component.onImageClick(5);
+    expect(mockGalleryService.addFavorite).toHaveBeenCalledWith(5);
+  })
+
+  it('should init', () => {
+    component.ngOnInit();
+    expect(mockGalleryService.getPage).toHaveBeenCalled();
+  });
+
+  it('should call api and subscribe to an observable', () => {
+    const observable = cold('a', { a: [1, 2, 3]});
+    mockGalleryService.getPage.and.returnValue(observable);
+    spyOn(observable, "subscribe");
+    component.getImages();
+
+    expect(observable.subscribe).toHaveBeenCalled();
   });
 });
--- src/app/gallery/components/photos/photos.component.ts
+++ src/app/gallery/components/photos/photos.component.ts
@@ -1,23 +1,74 @@
-import { Component, OnInit } from '@angular/core';
-import {GalleryService} from "../../services/gallery.service";
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChildren} from '@angular/core';
+import { GalleryService } from "../../services/gallery.service";
+import {delay, finalize, Subject, takeUntil, tap} from "rxjs";
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.scss']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnInit, AfterViewInit, OnDestroy {
+  @ViewChildren('viewCard', { read: ElementRef })
+  public viewCards!: QueryList<ElementRef>;
+
   public loading = false;
   public indexList: number[] = [];
+  public currentPage: number = 1;
+  private takeUntil$: Subject<void> = new Subject();
+  private observer!: IntersectionObserver;
 
 
   constructor(private galleryService: GalleryService) { }
 
-  ngOnInit(): void {
-    this.indexList = [...Array(10).keys()]
+  public ngOnInit(): void {
+    this.getImages();
   }
 
-  onImageClick(id: number) {
+  public ngAfterViewInit(): void {
+    this.initObserver();
+    this.viewCards.changes.pipe(
+      takeUntil(this.takeUntil$)
+    ).subscribe((queryList: QueryList<ElementRef>) => {
+      this.observer.observe(queryList.last.nativeElement);
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.takeUntil$.next();
+    this.takeUntil$.complete();
+  }
+
+  public onImageClick(id: number): void {
     this.galleryService.addFavorite(id);
+  }
+
+  public getImages(): void {
+    this.galleryService.getPage(this.currentPage).pipe(
+      takeUntil(this.takeUntil$),
+      tap(() => this.loading = true),
+      delay(this.getRandomDelay()),
+      finalize(() => this.loading = false),
+    ).subscribe((data: number[]) => {
+      this.currentPage += 1;
+      this.indexList.push(...data);
+    });
+  }
+
+  private observerCallback(entries: IntersectionObserverEntry[]): void {
+    if(entries[0].isIntersecting) {
+      this.getImages();
+      this.observer.disconnect();
+    }
+  }
+
+  private initObserver(): void {
+    const options = {
+      threshold: 0.95
+    }
+    this.observer = new IntersectionObserver(this.observerCallback.bind(this), options);
+  }
+
+  private getRandomDelay(): number {
+    return Math.floor(Math.random() * 100 + 200);
   }
 }
